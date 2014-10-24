109a9ccaed3cc95df61ee7ff2a28dc027a8f6829
Bas Beelen <bjgbeelen@gmail.com>
2014-10-24 14:07:24 +0200

    Basic User moves are registered

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,8 +3,8 @@ angular.module('ticTacToeExperimentApp')
     var self = this;
 
     self.gameState = {
-      'grid': [
-        [' ', ' ', ' '],
+      grid: [
+        [' x', ' ', ' '],
         [' ', ' ', ' '],
         [' ', ' ', ' ']
       ],
@@ -14,7 +14,20 @@ angular.module('ticTacToeExperimentApp')
 
     self.renderedGameState = [];
 
+    self.registerMove = function(index) {
+      var xAxis = Math.floor(index / 3);
+      var yAxis = index - 3*xAxis;
+      self.gameState.grid[xAxis][yAxis] = self.gameState.turn;
+      self.verifyWinningMove(xAxis, yAxis, self.gameState.turn);
+      self.renderGameState();
+    }
+
+    self.verifyWinningMove = function(xAxis, yAxis, char) {
+      
+    }
+
     self.renderGameState = function(){
+      self.renderedGameState=[];
       angular.forEach(self.gameState.grid, function(value){
         angular.forEach(value, function(value){
           self.renderedGameState.push(value);
