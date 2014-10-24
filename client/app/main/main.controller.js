16ed0c2c17d0589758c57c0b0f647918ee50a2bd
Jeroen Willemsen <jwillemsen@xebia.com>
2014-10-24 12:55:09 +0200

    added renderedgamestate for now

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -12,8 +12,18 @@ angular.module('ticTacToeExperimentApp')
       'o': 'nameplayer2',
       'turn': 'x'};
 
+    self.renderedGameState = [];
+
+    self.renderGameState = function(){
+      angular.forEach(self.gameState.grid, function(value){
+        angular.forEach(value, function(value){
+          self.renderedGameState.push(value);
+        });
+      });
+    };
     self.message = '';
 
+    self.renderGameState();
     mySocket.on('news', function (data) {
       self.message = data;
       mySocket.emit('my other event', { my: 'data' });
