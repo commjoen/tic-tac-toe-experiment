412a9ea48b55f1155ef6836cfa1668215ccab301
Jeroen Willemsen <jwillemsen@xebia.com>
2014-10-24 10:57:45 +0200

    first data structure and visualisation

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,6 +4,10 @@ angular.module('ticTacToeExperimentApp')
   .controller('MainCtrl', function ($scope, $http) {
     var self =this;
 
+    self.gameState = {'grid' : [["1", "2", "3"],["4", "5", "6"],["7", "8", "9"]],
+    'x': 'nameplayer1',
+    'o': 'nameplayer2',
+    'turn': 'x'};
 
     $http.get('/api/things').success(function(awesomeThings) {
       self.awesomeThings = awesomeThings;
