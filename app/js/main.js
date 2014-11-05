bb03d5e248cd682a67c3988cbf4632cb824d0315
Jeroen Willemsen <jwillemsen@xebia.com>
2014-11-05 20:31:47 +0100

    added directional light and spotlight

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@ require({
   // three.js should have UMD support soon, but it currently does not
   shim: { 'vendor/three': { exports: 'THREE' },
           'vendor/trackballcontrols': { deps: ['vendor/three'], exports: 'THREE' },
+          'vendor/orbitcontrols': { deps: ['vendor/three'], exports: 'THREE' },
           'vendor/detector': { exports: 'Detector' }
    }},[
   'vendor/trackballcontrols', 'vendor/detector'
@@ -11,10 +12,10 @@ require({
 
 
 
-  THREE.TrackballControls.prototype = Object.create(THREE.EventDispatcher.prototype);
+  //THREE.TrackballControls.prototype = Object.create(THREE.EventDispatcher.prototype);
 
   var camera, scene, renderer, controls;
-  var geometry, material, mesh, directionalLight;
+  var geometry, material, mesh, directionalLight, ground, spotlight;
 
   init();
   animate();
@@ -50,20 +51,52 @@ require({
       color: 0xffffff,
       map: THREE.ImageUtils.loadTexture('broek.jpg') // load a Wehkamp image, as texture of the cube
     });
-
     mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set (300, 300, 300);
+    mesh.castShadow = true;
+    mesh.receiveShadow = false;
     scene.add(mesh);
 
+    var groundgeometry = new THREE.PlaneGeometry( 5000, 2000, 3200 );
+    var groundmaterial = new THREE.MeshLambertMaterial( {color: 0x999999, side: THREE.DoubleSide} );
+    ground = new THREE.Mesh( groundgeometry, groundmaterial );
+    ground.castShadow = false;
+    ground.receiveShadow = true;
+    scene.add( ground );
+
+
+
     // added directional light
-    directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
-    directionalLight.position.set(1, 3, 24).normalize();
+    directionalLight = new THREE.DirectionalLight(0xffffff, 0.05);
+    directionalLight.position.set(600,600,600).normalize();
     scene.add(directionalLight);
     var ambientLight = new THREE.AmbientLight(0x444444);
     scene.add(ambientLight);
 
+spotLight = new THREE.SpotLight( 0xFFAA88 );
+  spotLight.target.position.set( 100, 200, -500 );
+  spotLight.position.set(600,600,600);
+  spotLight.shadowCameraNear  = 100.01;
+  spotLight.castShadow    = true;
+  spotLight.shadowDarkness  = 0.5;
+  spotLight.shadowCameraVisible = true;
+// console.dir(spotLight)
+ spotLight.shadowMapWidth = 1024;
+ spotLight.shadowMapHeight  = 1024;
+  scene.add( spotLight );
+
+
+
     renderer = Detector.webgl? new THREE.WebGLRenderer(): new THREE.CanvasRenderer();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMapEnabled = true;
+    renderer.shadowMapSoft = true;
+    renderer.shadowMapType = THREE.PCFSoftShadowMap;
+
+
+
+
 
     document.body.appendChild(renderer.domElement);
 
